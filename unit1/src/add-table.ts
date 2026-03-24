3be3ce6c0025a8ba1d36f7a5664c311f87b75933
Eyder Rios <eyder.rios@gmail.com>
2026-03-24 17:56:11 -0300

    Aula 2026-03-24

---
+++ unit1/src/add-table.ts
@@ -0,0 +1,9 @@
+import { inputNumber, print } from './stdio'
+
+print('TABUADA DE ADIÇÃO')
+
+const number = inputNumber('Tabuada de: ')
+
+for (let k = 1; k <= 10; k++) {
+  print(`${k} + ${number} = ${number + k}`)
+}
---
+++ unit1/src/fruits.ts
@@ -0,0 +1,19 @@
+import { print } from './stdio'
+
+const fruits = ['Laranja', 'Banana', 'Maçã', 'Goiaba', 'Uva', 'Jaca', 'Melancia']
+
+print('for: tipo 1')
+for (let i = 0; i < fruits.length; i++) {
+  print(`${i}: ${fruits[i]}`)
+}
+
+print('\nfor: tipo 2')
+let index = 0
+for (let fruit of fruits) {
+  print(`${index++}: ${fruit}`)
+}
+
+print('\nfor: tipo 3')
+for (let index in fruits) {
+  print(`${index}: ${fruits[index]}`)
+}
