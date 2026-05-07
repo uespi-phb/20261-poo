6eb6824c745b3410880d1b3e1c6251c54a44e9b7
Eyder Rios <eyder.rios@gmail.com>
2026-05-07 16:15:41 -0300

    update repo structure

--- hello/build/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var count;
-var city = "Parnaíba";
-var single;
-count = 2026;
-console.log("count =", count);
-console.log("city =", city);
-//# sourceMappingURL=main.js.map
--- samples/lint-staged.config.mjs
+++
@@ -1,5 +0,0 @@
-export default {
-  '*.{ts,js,mjs,cjs}': ['prettier --write'],
-  '*.{json,md,yml,yaml}': ['prettier --write'],
-  '.env*': ['prettier --write'],
-}
