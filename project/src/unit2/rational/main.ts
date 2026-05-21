c51b9132a52424f7e0c78f8a516b9803726dc1ea
Eyder Rios <eyder.rios@gmail.com>
2026-05-21 17:48:51 -0300

    Aula 2026-05-21

--- project/src/unit2/rational/main.ts
+++ project/src/unit2/rational/main.ts
@@ -1,11 +1,43 @@
 import { Rational } from './rational'
 import { RationalSet } from './rational-set'
 
-const s1 = new RationalSet()
+const r = [
+  new Rational(-1, 4),
+  new Rational(5, 2),
+  new Rational(-1, 2),
+  new Rational(-4, 7),
+  new Rational(8, 3),
+  new Rational(1, 8),
+]
 
+const s0 = new RationalSet(r)
+s0.show('s0')
+
+const s1 = new RationalSet()
 s1.add(new Rational(-1, 4))
 s1.add(new Rational(5, 2))
 s1.add(new Rational(8, 3))
-s1.add(new Rational(5, 2))
 
-console.log(s1.toString())
+const s2 = new RationalSet()
+s2.add(new Rational(-1, 2))
+s2.add(new Rational(-4, 7))
+s2.add(new Rational(8, 3))
+s2.add(new Rational(1, 8))
+
+const s3 = new RationalSet([7, 9, 4, -5, 2])
+s3.show('s3')
+
+s1.show('s1')
+s2.show('s2')
+
+const u = s1.union(s2)
+u.show('s1 U s2')
+
+const i = s1.intersection(s2)
+i.show('s1 ^ s2')
+
+const d1 = s1.difference(s2)
+d1.show('s1 - s2')
+
+const d2 = s2.difference(s1)
+d2.show('s2 - s1')
--- project/src/unit2/rational/rational-set.ts
+++ project/src/unit2/rational/rational-set.ts
@@ -1,10 +1,28 @@
 import { Rational } from './rational'
 
+function isRationalArray(array: unknown[]): boolean {
+  return array.length === 0 || array[0] instanceof Rational
+}
+
+function isNumberArray(array: unknown[]): boolean {
+  return array.length === 0 || typeof array[0] === 'number'
+}
+
 export class RationalSet {
   private readonly data: Rational[]
 
-  constructor() {
-    this.data = []
+  constructor(source?: RationalSet | number[] | Rational[]) {
+    if (source === undefined) {
+      this.data = []
+      return
+    }
+    if (source instanceof RationalSet) {
+      this.data = [...source.data]
+    } else if (isRationalArray(source)) {
+      this.data = [...(source as Rational[])]
+    } else {
+      this.data = (source as number[]).map((num) => new Rational(num, 1))
+    }
   }
 
   toString(): string {
@@ -22,10 +40,60 @@ export class RationalSet {
     return result
   }
 
-  add(r: Rational): void {
-    for (const elem of this.data) {
-      if (r.equals(elem)) return
+  show(setName?: string): void {
+    if (setName) {
+      console.log(`${setName} = ${this.toString()}`)
+    } else {
+      console.log(this.toString())
     }
+  }
+
+  constains(r: Rational): boolean {
+    for (const elem of this.data) {
+      if (r.equals(elem)) return true
+    }
+    return false
+  }
+
+  add(r: Rational): void {
+    if (this.constains(r)) return
     this.data.push(r)
+  }
+
+  remove(r: Rational): void {
+    for (let i = 0; i < this.data.length; i++) {
+      if (r.equals(this.data[i]!)) {
+        this.data.splice(i, 1)
+        break
+      }
+    }
+  }
+
+  union(other: RationalSet): RationalSet {
+    const unionSet = new RationalSet(this)
+    for (const elem of other.data) {
+      unionSet.add(elem)
+    }
+    return unionSet
+  }
+
+  intersection(other: RationalSet): RationalSet {
+    const intersectionSet = new RationalSet()
+    for (const elem of this.data) {
+      if (other.constains(elem)) {
+        intersectionSet.add(elem)
+      }
+    }
+    return intersectionSet
+  }
+
+  difference(other: RationalSet): RationalSet {
+    const diffSet = new RationalSet()
+    for (const elem of this.data) {
+      if (!other.constains(elem)) {
+        diffSet.add(elem)
+      }
+    }
+    return diffSet
   }
 }
